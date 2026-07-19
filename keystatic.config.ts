2f6eb965c999e2fe4e74fb1cfd0f54fe1a13fa8f
AlvaroAjg <alvaroalejandroj.03@gmail.com>
2026-07-19 01:37:28 -0400

    fix: corrige variables de entorno y añade repo

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,25 +1,28 @@
 import { config, fields, collection } from '@keystatic/core';
 
 export default config({
-  // 1. MODO LOCAL: Los datos se guardarán en tu compu por ahora
+ 
   storage: {
-    kind: 'local',
-  },
-  // 2. LAS COLECCIONES: Aquí definimos la galería
+  
+      kind: import.meta.env.PROD ? 'github' : 'local',
+  
+      repo: 'AlvaroAjg/pbpetrus-site', 
+},
+  
   collections: {
     obras: collection({
       label: 'Obras y Trabajos',
       slugField: 'title',
-      path: 'src/content/obras/*', // Aquí se crearán los archivos de datos
-      format: { data: 'json' }, // Guardaremos todo en formato JSON
+      path: 'src/content/obras/*',
+      format: { data: 'json' },
       schema: {
         title: fields.slug({ 
           name: { label: 'Título de la ilustración' } 
         }),
         image: fields.image({
           label: 'Imagen de la obra',
-          directory: 'public/obras', // La carpeta donde se guardarán los JPG/PNG
-          publicPath: '/obras/'      // La ruta pública para leerlas en la web
+          directory: 'public/obras', 
+          publicPath: '/obras/'      
         }),
         isFeatured: fields.checkbox({
           label: '¿Es una obra destacada?',
