27ef3c98aa1be5716b04db68c201e41ffb000a98
AlvaroAjg <alvaroalejandroj.03@gmail.com>
2026-07-19 01:15:20 -0400

    fix: agrega adaptador de Vercel para soportar SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,18 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import keystatic from '@keystatic/astro'; // <-- 1. Importamos Keystatic
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
   integrations: [
     react(),
     keystatic() 
-  ]
+  ],
+
+  adapter: vercel()
 });
