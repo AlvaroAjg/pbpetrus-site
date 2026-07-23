0bc31c3cb3c907ab8dd611cdf62a91eaf2b5bb2c
AlvaroAjg <alvaroalejandroj.03@gmail.com>
2026-07-23 09:14:27 -0400

    fix : Navegador

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
-import keystatic from '@keystatic/astro'; // <-- 1. Importamos Keystatic
+import keystatic from '@keystatic/astro'; 
 
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://pbpetrus-site.vercel.app', // <-- 1. Definimos tu dominio oficial
+  output: 'server', // <-- 2. Habilitamos el procesamiento del lado del servidor (SSR)
+  
   vite: {
     plugins: [tailwindcss()]
   },
